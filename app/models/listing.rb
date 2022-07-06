d5aec9f4076e41a4844bbcc8f02bbaf813d2781c
Connor <10349@coderacademy.edu.au>
2022-07-06 17:09:53 +1000

    added condition and genre enums to listing model file

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,30 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :product_type
   has_rich_text :description
+
+  enum condition: {
+    poor: 1,
+    used: 2,
+    good_condition: 3,
+    brand_new: 4
+  }
+
+  enum genre: {
+    rock: 1,
+    electronic: 2,
+    hip_hop: 3,
+    jazz: 4,
+    folk: 5,
+    classical: 6,
+    rhythm_and_blues: 7,
+    country: 8,
+    pop: 9,
+    indie_rock: 10,
+    reggae: 11,
+    blues: 12,
+    international: 13,
+    other: 14
+  }
+
+
 end
