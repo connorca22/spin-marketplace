fb66e5ab1b1f19993a87aeff25da64ecdfa99a68
connorca22 <99699743+connorca22@users.noreply.github.com>
2022-07-17 13:49:02 +1000

    Revert "Dev"

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,4 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, except: [:home]
-  
   def home
   end
-
-  def success
-  end
-
-  #stores bought, sold and active listings in instance variable for use in view.
-  def profile
-    @bought_products = current_user.bought_orders
-    @sold_listings = current_user.sold_orders
-    @unsold_listings = current_user.listings.where(sold: false)
-  end 
 end
