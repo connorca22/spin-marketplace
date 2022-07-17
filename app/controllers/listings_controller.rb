6f8ebbe1ee7caec56b960511a8a90791d8c93b89
Connor <10349@coderacademy.edu.au>
2022-07-17 13:16:23 +1000

    added classes to all viewable views, then added global styling in application.css. Also added an authetnicate_user in pages controller so an error wasn't thrown when you typed in pages/profile into search bar

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,28 +1,36 @@
 class ListingsController < ApplicationController
+  #runs all relevant methods before any action. 
   before_action :set_listing, only: %i[ show edit update destroy new_order]
   before_action :set_form_vars
   before_action :authenticate_user!, except: [:index, :show]
   before_action :authorize_user, only: [:edit, :update, :destroy] 
 
   # GET /listings or /listings.json
+  # Navigates to the listings page. Creates instance variable and stores all listings inside it. 
   def index
     @listings = Listing.all
   end
 
   # GET /listings/1 or /listings/1.json
+  # Navigates to the listing show page
   def show
   end
 
   # GET /listings/new
+  #Navigates to the new listing page/form. Creates a new listing and stores it in @listing (not saved to database yet)
   def new
     @listing = Listing.new
   end
 
   # GET /listings/1/edit
+  # Navigates to the edit listing page/form. 
   def edit
   end
 
   # POST /listings or /listings.json
+  # If a post request is received to create new listings, then create a new record in listings table with listing params that have been 
+  # passed in. Store this in a @listing variable. Then update the user_id on listing to the user creating the listing. Then advise the user
+  # if the listing creation was successful or not. 
   def create
     @listing = Listing.new(listing_params)
     @listing.user = current_user
@@ -39,6 +47,7 @@ class ListingsController < ApplicationController
   end
 
   # PATCH/PUT /listings/1 or /listings/1.json
+  #If a patch/put request is received on a listing show page, then redirect to that page and advise user if edit was successsful or not. 
   def update
     respond_to do |format|
       if @listing.update(listing_params)
@@ -52,6 +61,7 @@ class ListingsController < ApplicationController
   end
 
   # DELETE /listings/1 or /listings/1.json
+  # deletes the selected record in listings table and responds with a success message when deleted.  
   def destroy
     @listing.destroy
 
@@ -61,7 +71,9 @@ class ListingsController < ApplicationController
     end
   end
 
-
+#executed when user clicks the purchase button on listing. 
+#creates a record in the Order table and sets its listing id, seller id, and buyer id. 
+#also updates the current record in listing tables sold value to true, and redirects to success page. 
   def new_order 
     Order.create(
       listing_id: @listing.id, 
@@ -73,13 +85,14 @@ class ListingsController < ApplicationController
   end 
 
   private
-    
+    #defines instance variables and stores Product Types, Conditions and Genres inside so they can be accessed in the view. 
     def set_form_vars 
       @product_types = ProductType.all 
       @conditions = Listing.conditions.keys
       @genres = Listing.genres.keys 
     end 
 
+    #Throws alert if user attempts to edit or delete a listing beloning to someone else. Redirects to listings page. 
     def authorize_user 
       if @listing.user_id != current_user.id 
         flash[:alert] = "You do not own this listing." 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, except: [:home]
+  
   def home
   end
 
   def success
   end
 
-  #stores bought, sold and active listings in instance variable for use in view. 
+  #stores bought, sold and active listings in instance variable for use in view.
   def profile
     @bought_products = current_user.bought_orders
     @sold_listings = current_user.sold_orders
